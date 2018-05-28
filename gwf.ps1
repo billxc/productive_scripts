44531c52246028167c6e4d64d2a3927ac06b0d66
wuxiaochen2 <wuxiaochen3@jd.com>
2018-05-28 15:32:17 +0800

    modify

--- gwf.ps1
+++ gwf.ps1
@@ -3,24 +3,70 @@ function glog {
 }
 
 function gmcd {
-    $working = git branch |Select-String "\*"  | % {$_.Line.Trim("*", " ")}
-    echo "working is $working"
-    $dev = $args[0]
-    echo "dev is $dev"
-    $beta = $args[1]
-    echo "beta is $beta"
-    echo "git pull origin $working"
-    echo "git push origin $working"
+    param(
+        $dev,
+        $beta
+    )
 
-    echo "git checkout $dev"
-    echo "git pull origin $dev"
-    echo "git merge $working"
-    echo "git push origin $dev"
+    Write-Output "dev: $dev, beta $beta"
     
-    echo "git checkout $beta"
-    echo "git pull origin $beta"
-    echo "git merge $dev"
-    echo "git push origin $beta"
+    $working = git branch |Select-String "\*"  | % {$_.Line.Trim("*", " ")}
+    git fetch origin
+
+    $match = git branch --all | Select-String "\s+remotes/origin/$working$" 
+    # update working and push
+    if ($match.count -eq 1 ){
+        Write-Output "remote $working exist, pull first"
+        git pull origin $working
+    }
+    git push origin $working
     
-    echo "git checkout $working"
+
+    # checkout & update dev 
+    # merge working to dev
+    # push dev
+
+    tryCheckout($dev)
+    git merge $working
+    git push origin $dev
+
+    # checkout & update beta
+    # merge dev to beta
+    # push beta
+    tryCheckout($beta)
+    git merge $dev
+    git push origin $beta
+    
+    git checkout $working
+}
+
+function tryCheckout($branch) {
+    $localMatch = git branch --all | Select-String "\s+$branch$"
+    $remoteMatch = git branch --all | Select-String "\s+remotes/origin/$branch$"
+    $local = $localMatch.count
+    $remote = $remoteMatch.count
+    # 本地有，远程有 ,checkout pull
+    # 本地无，远程有 ,checkout remote
+    # 本地无，远程无 ,checkout -b only
+    # 本地有，远程无 ,checkout only
+    if (($local -eq 1) -and ($remote -eq 1) ) {
+        Write-Output "$branch 本地有，远程有 ,checkout pull"
+        git checkout $branch
+        git pull origin $branch
+    }
+    elseif (($local -eq 0 ) -and ($remote -eq 1) ) {
+        Write-Output "$branch 本地无，远程有 ,checkout remote"
+        git checkout origin/$branch -b $branch
+    }
+    elseif (($local -eq 0) -and ($remote -eq 0) ) {
+        Write-Output "$branch 本地无，远程无 ,checkout -b only"
+        git checkout master -b $branch
+    }
+    elseif (($local -eq 1) -and ($remote -eq 0) ) {
+        Write-Output "$branch 本地有，远程无 ,checkout only"
+        git checkout $branch
+    }
+    else {
+        Write-Error "WTF the count doest not match, branch: $branch, local: $local, remote: $remote"
+    }
 }
